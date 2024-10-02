c553453417ad8db31ea79643dd8355665c7f1899
Parsa Bordbar <parsab71@gmail.com>
2024-10-03 03:19:30 +0330

    feat: useGetConfig hook for handleing config data fetching

--- hooks/useGetConfig.ts
+++ hooks/useGetConfig.ts
@@ -2,16 +2,16 @@ import { useEffect, useState } from "react";
 import { spaceApi } from "../api/base";
 
 
-const useGetConfigData = () => {
+const useGetConfigData = (slug: string) => {
     const [ configData, setConfigData ] = useState<any>();
     
     useEffect(() => {
-        fetchConfigData();
+        fetchConfigData(slug);
     }, [])
     
-    const fetchConfigData = async () => {
+    const fetchConfigData = async (slug: string) => {
         try {
-            const response = await spaceApi.get(`/rooms/getUiConfig`);
+            const response = await spaceApi.get(slug);
             setConfigData(response.data);
             console.log("This is Response from Hook, ", response);
          } catch (error) {
