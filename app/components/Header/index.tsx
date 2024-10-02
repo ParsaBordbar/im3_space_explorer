2d9135c485fa09378cdfa5545208f620d6c73f14
MohammadMosavat <42.octmst@gmail.com>
2024-10-02 13:36:10 +0330

    Sprate the components , now the header is component

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Graph from "/public/graph.svg";
+import NavBar from "../Navbar";
+
+const Header = () => {
+    return(
+        <header className="relative  w-full  flex-col justify-center">
+        <NavBar/>
+        <h1 className="text-white absolute left-[10%] md:left-[13%] md:top-[30%] text-2xl md:text-5xl font-bold">
+          Spacehall
+        </h1>
+        <Image src={Graph} width={5000} height={1000} className="object-cover" alt="graph" />
+      </header>
+    )
+}
+
+export default Header
