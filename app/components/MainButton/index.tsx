ee1c239226a480a6e91ae9ab9bef28cf2d5d2bed
MohammadMosavat <42.octmst@gmail.com>
2024-10-02 21:29:12 +0330

    Update : Update the MainButton component , ADD 4 type of button that we use it

--- app/components/MainButton/index.tsx
+++ app/components/MainButton/index.tsx
@@ -1,44 +1,33 @@
 "use client";
-import {
-  ButtonHTMLAttributes,
-  FunctionComponent,
-  useCallback,
-  useMemo,
-} from "react";
-type TButton = ButtonHTMLAttributes<HTMLButtonElement> & {
-  value?: string;
-  className?: string;
-  iconSrc?: FunctionComponent;
-  submit?: boolean;
-  simple?:boolean
-};
-
+import { TButton } from "@/app/types";
+import Image from "next/image";
+import { useCallback, useMemo } from "react";
 
 const MainButton = (props: TButton) => {
   const declearModleOfButton = useMemo(() => {
     if (props.submit) {
       return "bg-black hover:bg-[#121212]";
-    } else if(props.simple){
-        return "hover:bg-[#2a2a2a] py-4 bg-[#1e1e1e] border border-[#ffffff] border-opacity-10"
+    } else if (props.simple) {
+      return "hover:bg-[#2a2a2a] py-2 md:py-4 bg-[#1e1e1e] border border-[#ffffff] border-opacity-10";
+    }else if(props.freez){
+      return "bg-[#1E1E1E] cursor-default"
+    } else if(props.pro){
+      return "bg-[#1F8CF9] hover:bg-sky-700 font-bold"
     }
   }, []);
-  
+
   const checkIcon = useCallback(() => {
-    if (props.iconSrc) return <props.iconSrc />;
+    if (props.iconSrc) return <Image src={props.iconSrc} height={16} width={16} alt=""/>;
   }, []);
   return (
     <button
       {...props}
-      className={`${
-        props.className
-      }  ${declearModleOfButton} ${
-        props.iconSrc && "flex items-center gap-2"
-      } text-center text-white ` }
+      className={`${props.className}  ${declearModleOfButton} ${
+        props.iconSrc && "flex items-center gap-1"
+      } text-center text-white `}
     >
       {checkIcon()}
-      <span className={``}>
-        {props.value}
-      </span>
+      <span className={``}>{props.value}</span>
     </button>
   );
 };
