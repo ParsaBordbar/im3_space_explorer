62f481f161570fdea5fdf4c4abc8e12a711fa1db
MohammadMosavat <42.octmst@gmail.com>
2024-10-05 18:00:01 +0330

    change the layout of te layput of filter

--- app/filters/layout.tsx
+++ app/filters/layout.tsx
@@ -1,18 +1,11 @@
 import { ReactNode } from "react";
-import React from "react";
-import Navbar from "../components/Navbar";
+import NavBar from "../components/Navbar";
 
 const LayoutOfTagsPage = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <Navbar />
-      
-      <main className="grid my-10 w-11/12 md:w-[75%] gap-10 mx-auto grid-cols-2">
-          <h1 className="text-white text-lg font-bold col-span-full">
-            Explore Spaces
-          </h1>
-        {children}
-      </main>
+      <NavBar />
+      {children}
     </>
   );
 };
