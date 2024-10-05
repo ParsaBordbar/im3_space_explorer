13218ab0160b5bfdb51cc357e9df9d2419b10dd0
MohammadMosavat <42.octmst@gmail.com>
2024-10-05 15:53:40 +0330

    Styles: remove the margin of detailMiniBox

--- app/components/DetailMiniBox/index.tsx
+++ app/components/DetailMiniBox/index.tsx
@@ -6,7 +6,7 @@ const DetailMiniBox = ({ icon, value, className, title }: DetailMiniBoxType) => 
   return (
     <li
       title={`${title}`}
-      className={`flex items-center ${className}  bg-[#373737] rounded-lg px-1 py-0.5 gap-1 w-fit mx-2`}
+      className={`flex items-center ${className}  bg-[#373737] rounded-lg px-1 py-0.5 gap-1 w-fit `}
     >
       <Image src={icon} width={14} height={14} alt="" />
       <p className="text-white">{value}</p>
