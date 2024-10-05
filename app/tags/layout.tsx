a2b4789b61f5120fea030ebdbc7c8a02f2faf511
MohammadMosavat <42.octmst@gmail.com>
2024-10-05 17:19:31 +0330

    Update : change the layout of the layout of tags page

--- app/tags/layout.tsx
+++ app/tags/layout.tsx
@@ -1,13 +1,11 @@
 import { ReactNode } from "react";
-import Header from "../components/Header";
+import NavBar from "../components/Navbar";
 
 const LayoutOfTagsPage = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <Header />
-      <main className="grid my-10 w-11/12 md:w-[75%] gap-10 mx-auto grid-cols-2">
-        {children}
-      </main>
+      <NavBar />
+      {children}
     </>
   );
 };
