e241373475b10b46a232158faefc309e15f2864a
MohammadMosavat <42.octmst@gmail.com>
2024-10-05 18:02:14 +0330

    responsie the filter page

--- app/filters/[filter]/page.tsx
+++ app/filters/[filter]/page.tsx
@@ -1,19 +1,21 @@
 "use client";
-import SpaceCard from '@/app/components/SpaceCard';
-import useGetConfigData from '@/hooks/useGetConfig';
-import React from 'react'
+import SpaceCard from "@/app/components/SpaceCard";
+import useGetConfigData from "@/hooks/useGetConfig";
+import React from "react";
 
 function Filter({ params }: { params: string }) {
-    const u = `/rooms/get-all-room-configs/sort?sort=${params.filter}`
-    console.log(u);
-    const uiData = useGetConfigData(u);
+  const u = `/rooms/get-all-room-configs/sort?sort=${params.filter}`;
+  console.log(u);
+  const uiData = useGetConfigData(u);
   return (
-    <>
+    <div className="flex flex-col my-10 w-11/12 md:w-[75%] mx-auto gap-4 items-start">
+      <h1 className="text-white text-lg font-bold capitalize">{params.filter} Spaces</h1>
+      <main className="grid gap-4 w-full mx-auto grid-cols-2">
         {uiData &&
           uiData.map((data: any) => {
             return (
               <SpaceCard
-                className={""}
+                className={"col-span-full md:col-span-1"}
                 logo_URL={data?.config?.ui?.logo}
                 slug={data?.slug}
                 key={data.slug}
@@ -26,11 +28,13 @@ function Filter({ params }: { params: string }) {
                 privateSpace={data?.config?.ui?.privateRoom}
                 roomUrl={data?.slug}
                 tags={data?.config?.ui?.tags}
+                verified={data?.config?.verified}
               />
             );
           })}
-    </>
-  )
+      </main>
+    </div>
+  );
 }
 
-export default Filter
+export default Filter;
