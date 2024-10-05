d0664a18adb0317cd3da69d631bce25c6d426781
MohammadMosavat <42.octmst@gmail.com>
2024-10-05 15:53:07 +0330

    Update : tag component

--- app/components/Tag/index.tsx
+++ app/components/Tag/index.tsx
@@ -1,15 +1,17 @@
-import { TagType } from '@/app/types'
-import Link from 'next/link'
-import React from 'react'
+import { TagType } from "@/app/types";
+import Link from "next/link";
+import React from "react";
 
-function Tag({tag}: TagType) {
+function Tag({ tag }: TagType) {
   return (
-    <Link href={`/${tag}`}>
-        <li className="flex col-span-1 text-xs p-1.5 w-full text-white bg-[#373737] mx-2 my-.5 justify-center items-center rounded-lg">
-        {tag}
-        </li>
+    <Link href={`/tags/${tag}`}>
+      <li
+        className={`text-xs w-fit text-white hover:underline underline-offset-4`}
+      >
+        #{tag}
+      </li>
     </Link>
-  )
+  );
 }
 
-export default Tag
+export default Tag;
