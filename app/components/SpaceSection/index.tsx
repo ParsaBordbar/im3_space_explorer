08647fa772c09c5480c4226288d8573caa34cba1
parsaBordbar <parsab71@gmail.com>
2024-10-10 13:30:03 +0330

    fix: handle type fort SpaceSection

--- app/components/SpaceSection/index.tsx
+++ app/components/SpaceSection/index.tsx
@@ -2,17 +2,16 @@
 import useGetConfigData from '@/hooks/useGetConfig'
 import React from 'react'
 import SpaceCard from '../SpaceCard'
-import { SpaceSectionType } from '@/app/types'
+import { dataType, SpaceSectionType } from '@/app/types'
 
 function SpaceSection({search}: SpaceSectionType) {
   const uiData = useGetConfigData(`/rooms/get-all-room-configs/sort?sort=all`)
-  
   console.log('This is search in space Section', search);
 
   return (
     <section className="grid col-span-full grid-cols-2 gap-4">
       {
-        uiData?.map((data: any) => {
+        uiData?.map((data: dataType) => {
           const regex = new RegExp(search, 'i'); 
           const matchesSearch = regex.test(data.slug);
 
@@ -26,7 +25,7 @@ function SpaceSection({search}: SpaceSectionType) {
                 className={"md:col-span-1 col-span-full"}
                 key={data.slug}
                 slug={data?.slug} roomUrl={data?.slug}
-                tags={data?.config?.ui?.tags} verified={data?.config?.verified}                />
+                tags={data?.config?.ui?.tags} verified={data?.config?.verified}/>
             ) : null
           );
         })
