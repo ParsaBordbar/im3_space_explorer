6b0b4056f9a9770d0a0c9b03b4bedb26424ce799
MohammadMosavat <42.octmst@gmail.com>
2024-10-20 16:34:00 +0330

    change the font size of the tag component in mobile size

--- app/components/Tag/index.tsx
+++ app/components/Tag/index.tsx
@@ -6,7 +6,7 @@ function Tag({ tag }: TagType) {
   return (
     <Link href={`/tags/${tag}`}>
       <li
-        className={`text-xs w-fit font-Nunito hover:underline underline-offset-4 opacity-70 text-[#F4F4F4]`}
+        className={`text-sm md:text-xs w-fit font-Nunito hover:underline underline-offset-4 opacity-70 text-[#F4F4F4]`}
       >
         #{tag}
       </li>
