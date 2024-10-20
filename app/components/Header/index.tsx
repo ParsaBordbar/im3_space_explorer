fa1c74eb59e90015d37f133cf1fc8ef8adc2f916
MohammadMosavat <42.octmst@gmail.com>
2024-10-20 16:35:43 +0330

    Fix the noise effect svg in header component

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,14 +1,32 @@
+import Image from "next/image";
 import NavBar from "../Navbar";
-import NoiseEffect from "/public/noiseEffect.svg";
+import NoiseEffect from "/public/noiseEffect.svg?url";
+import NoiseEffect3 from "/public/noiseEffect3.svg?url";
 
-const Header = () => {
+const Header = ({ mode }: { mode: "justNavbar" }) => {
   return (
-    <header className="relative top-0  w-full h-[170px]  flex-col justify-center">
-      <NoiseEffect className="w-fit max-md:hidden mx-auto absolute left-[12.2%] -z-10 top-0" />
+    <header className="relative  w-full h-[170px]  flex-col justify-center">
+      {/* <NoiseEffect className=" mx-auto  absolute left-0 right-0 bottom-0 transform translate-x-50  -z-10 top-0" /> */}
+      <Image
+        className="object-cover max-md:hidden left-0 right-0 top-0 w-full -z-10 absolute"
+        src={NoiseEffect}
+        width={5000}
+        height={1}
+        alt=""
+      />
+      <Image
+        className="object-cover md:hidden left-0 right-0 top-0 w-full -z-10 absolute"
+        src={NoiseEffect3}
+        width={5000}
+        height={1}
+        alt=""
+      />
       <NavBar />
-      <h1 className="font-SpaceGrotesk text-white absolute left-[4%] md:left-[12%] top-[100%] md:top-[90%] text-4xl md:text-6xl font-bold">
-        Spacehall
-      </h1>
+      {mode != "justNavbar" && (
+        <h1 className="font-SpaceGrotesk text-white absolute left-[4%] md:left-[12%] top-[100%] md:top-[90%] text-4xl md:text-6xl font-bold">
+          Spacehall
+        </h1>
+      )}
     </header>
   );
 };
