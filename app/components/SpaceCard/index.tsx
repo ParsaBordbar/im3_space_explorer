b5dc854140200605d866ebd9d2d51d76f70ef09c
MohammadMosavat <42.octmst@gmail.com>
2024-10-20 14:30:52 +0330

    fix the gaps and the layer of the noise effect

--- app/components/SpaceCard/index.tsx
+++ app/components/SpaceCard/index.tsx
@@ -25,11 +25,11 @@ const SpaceCard = ({
   return (
     <Link
       href={!roomUrl ? "/im3" : `/${handelRoomURL(roomUrl)}`}
-      className={`${className} relative cursor-pointer bg-box-space rounded-2xl flex flex-col gap-7 p-8`}
+      className={`${className} relative cursor-pointer bg-box-space rounded-2xl flex flex-col gap-7 p-4 md:p-8`}
     >
       <div className="flex items-center gap-4">
-        <div className="bg-[#131313] bg-opacity-70 w-[80px] h-full  flex justify-center items-center  rounded-2xl ">
-          <Image src={logo_URL} width={48} height={48} alt="logo" />
+        <div className="bg-[#131313] bg-opacity-70 w-[95px] h-[80px] flex items-center justify-center rounded-2xl ">
+          <Image src={logo_URL} width={44} height={40} alt="logo" />
         </div>
         <div className="flex flex-col w-full justify-between gap-3">
           <section className="flex items-center gap-2 ">
@@ -59,7 +59,7 @@ const SpaceCard = ({
           </section>
           <ul className="grid grid-cols-3 gap-4">
             <DetailMiniBox
-              className="col-span-1"
+              className=" col-span-2 sm:col-span-1"
               value={members}
               icon={Members}
               title={"max_member"}
