8956d23311dfe6d48e79dc3855d21eac309f7910
MohammadMosavat <42.octmst@gmail.com>
2024-10-20 16:36:39 +0330

    change the font size of the detail mini box component in mobile size

--- app/components/DetailMiniBox/index.tsx
+++ app/components/DetailMiniBox/index.tsx
@@ -12,8 +12,8 @@ const DetailMiniBox = ({
       title={`${title}`}
       className={`flex items-center ${className}  bg-[rgba(19,19,19,0.3)] rounded-md px-3 py-1 gap-2 w-fit `}
     >
-      <Image src={icon} width={16} height={16} alt="" />
-      <p className="text-white text-xl font-SpaceGrotesk">{value}</p>
+      <Image src={icon} className=" max-md:h-3 max-md:w-3" width={16} height={16} alt="" />
+      <p className="text-white text-xs md:text-xl font-SpaceGrotesk">{value}</p>
     </li>
   );
 };
