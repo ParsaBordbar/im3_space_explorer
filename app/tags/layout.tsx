c5b7527b64c114a717937182c746ec4b118ea3e7
MohammadMosavat <42.octmst@gmail.com>
2024-10-20 16:32:57 +0330

    Add Header include noise effect in layout of tags

--- app/tags/layout.tsx
+++ app/tags/layout.tsx
@@ -1,13 +1,10 @@
 import { ReactNode } from "react";
-import NavBar from "../components/Navbar";
-import React from "react";
-import NoiseEffect from "/public/noiseEffect.svg";
+import Header from "../components/Header";
 
 const LayoutOfTagsPage = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <NoiseEffect className="w-fit max-md:hidden mx-auto absolute left-[12.2%] -z-10 top-0" />
-      <NavBar />
+      <Header mode="justNavbar" />
       {children}
     </>
   );
