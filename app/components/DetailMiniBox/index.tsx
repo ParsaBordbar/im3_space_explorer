cf5f0cde14c8600a6d3f60e0e80a7dad965b3133
MohammadMosavat <42.octmst@gmail.com>
2024-10-21 13:39:08 +0330

    change the cursor in detail mini box

--- app/components/DetailMiniBox/index.tsx
+++ app/components/DetailMiniBox/index.tsx
@@ -10,7 +10,7 @@ const DetailMiniBox = ({
   return (
     <li
       title={`${title}`}
-      className={`flex items-center ${className}  bg-[rgba(19,19,19,0.3)] rounded-md px-3 py-1 gap-2 w-fit `}
+      className={`flex items-center ${className} cursor-default bg-[rgba(19,19,19,0.3)] rounded-md px-3 py-1 gap-2 w-fit `}
     >
       <Image src={icon} className=" max-md:h-3 max-md:w-3" width={16} height={16} alt="" />
       <p className="text-white text-sm md:text-xl font-SpaceGrotesk">{value}</p>
