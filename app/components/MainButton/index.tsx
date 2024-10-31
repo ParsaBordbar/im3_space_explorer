0a6a956f0ab7abf9cfd82500abfdde9b60a516db
MohammadMosavat <42.octmst@gmail.com>
2024-10-31 14:57:06 +0330

    UPdate the Main Button component

--- app/components/MainButton/index.tsx
+++ app/components/MainButton/index.tsx
@@ -5,25 +5,26 @@ import { useCallback, useMemo } from "react";
 
 const MainButton = (props: TButton) => {
   const declearModleOfButton = useMemo(() => {
-    if (props.submit) {
+    if (props.mode == "submit") {
       return "bg-black hover:bg-[#121212]";
-    } else if (props.simple) {
+    } else if (props.mode == "simple") {
       return "py-2  bg-[#1e1e1e] border border-[#ffffff] border-opacity-10";
-    }else if(props.freez){
-      return "bg-[#1E1E1E] cursor-default"
-    } else if(props.pro){
-      return "bg-[#036DE7] hover:bg-sky-700 font-bold"
+    } else if (props.mode == "freez") {
+      return "bg-[#1E1E1E] cursor-default";
+    } else if (props.mode == "pro") {
+      return "bg-[#036DE7] hover:bg-sky-700 font-bold";
     }
   }, []);
 
   const checkIcon = useCallback(() => {
-    if (props.iconSrc) return <Image src={props.iconSrc} height={16} width={16} alt=""/>;
+    if (props.iconSrc)
+      return <Image src={props.iconSrc} height={16} width={16} alt="" />;
   }, []);
   return (
     <button
       {...props}
       className={`${props.className}  ${declearModleOfButton} ${
-        props.iconSrc && "flex justify-center items-center gap-1"
+        props.iconSrc && "flex justify-center items-center gap-2"
       } text-center text-white `}
     >
       {checkIcon()}
