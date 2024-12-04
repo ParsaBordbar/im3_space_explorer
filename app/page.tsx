25e90b3522023b82d3c3f46f36d78052b9558b26
MohammadMosavat <42.octmst@gmail.com>
2024-12-04 19:22:17 +0330

    use SearchBar component and get the data from it

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,19 @@ import Search from "/public/search.svg";
 import Header from "./components/Header";
 import MainButton from "./components/MainButton";
 import SpaceSection from "./components/SpaceSection";
-import { useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import Link from "next/link";
 import Star from "/public/stars.svg?url";
 import Oldest from "/public/oldest.svg?url";
+import { useRouter } from "next/navigation";
+import SearchBar from "./components/SearchBar";
 export default function Home() {
   const [search, setSearch] = useState<string>("");
+  const { push } = useRouter();
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    console.log("Event value:", event.target.value);
-    setSearch(event.target.value);
+  const handleDataFromChild = (data: string) => {
+    setSearch(data);
   };
-
   return (
     <>
       <Header />
@@ -24,60 +24,9 @@ export default function Home() {
           <h1 className="text-white font-Nunito text-lg font-bold col-span-full">
             Explore Spaces
           </h1>
-          <form className="col-span-full flex md:flex-row flex-col border border-[#7B7B7B] rounded-[11px] items-center gap-2">
-            <div className="flex pl-3 items-center p-2 bg-transparent w-full rounded-2xl border border-[#7B7B7B] border-opacity-10">
-              <section className="flex  w-full items-center gap-2">
-                <Search />
-                <input
-                  onChange={handleChange}
-                  type="string"
-                  placeholder="Search your space"
-                  className="w-full font-Nunito outline-none text-white bg-transparent"
-                />
-              </section>
-            </div>
-            <div className="md:flex hidden px-4 py-2 bg-box-space rounded-[11px] items-center  w-full md:w-fit gap-2">
-              <Link
-                href={"/filters/newest"}
-                className="w-full group md:w-[104px]"
-              >
-                <MainButton
-                  value={"Newest"}
-                  mode="simple"
-                  iconSrc={Star}
-                  className="w-full bg-[#9C9898] transition-all duration-300 ease-in-out group-hover:[&>*]:scale-110  [&>*]:transition-all [&>*]:ease-in-out [&>*]:duration-200 !text-black font-Nunito rounded-lg"
-                />
-              </Link>
-              <Link href={"/filters/oldest"} className="w-full group md:w-[104px]">
-                <MainButton
-                  value={"Oldest"}
-                  mode="simple"
-                  iconSrc={Oldest}
-                  className="w-full bg-[#9C9898] transition-all duration-300 ease-in-out group-hover:[&>*]:scale-110  [&>*]:transition-all [&>*]:ease-in-out [&>*]:duration-200 !text-black font-Nunito rounded-lg"
-                />
-              </Link>
-            </div>
-          </form>
-          <div className="flex md:hidden rounded-[11px] items-center self-end w-fit gap-2">
-            <Link href={"/filters/newest"} className="w-[78px]">
-              <MainButton
-                value={"Newest"}
-                mode="simple"
-                iconSrc={Star}
-                className="w-full [&>span]:text-xs [&>img]:h-3 [&>img]:w-3 bg-[#9C9898] !text-black font-Nunito rounded-lg"
-              />
-            </Link>
-            <Link href={"/filters/oldest"} className=" w-[78px]">
-              <MainButton
-                value={"Oldest"}
-                mode="simple"
-                iconSrc={Oldest}
-                className="w-full  [&>span]:text-xs [&>img]:h-3 [&>img]:w-3 bg-[#9C9898] !text-black font-Nunito rounded-lg"
-              />
-            </Link>
-          </div>
+          <SearchBar filters sendDataToParent={handleDataFromChild} />
         </section>
-        <SpaceSection search={search} />
+        <SpaceSection search={search ?? ' '} />
       </main>
     </>
   );
