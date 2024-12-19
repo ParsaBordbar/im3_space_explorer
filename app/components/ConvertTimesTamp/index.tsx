4c3a0a94d57337486b0cda9f6319caa972054cd8
MohammadMosavat <42.octmst@gmail.com>
2024-12-19 13:14:56 +0330

    Add convert time tamp component to convert it the joinedAt data

---
+++ app/components/ConvertTimesTamp/index.tsx
@@ -0,0 +1,37 @@
+// Example Component: ConvertTimestamp.tsx
+import React from "react";
+
+const ConvertTimestamp = ({
+  time,
+  className,
+}: {
+  time: number | string;
+  className?: string;
+}) => {
+  const timestamp = time; // Unix timestamp
+  var formattedDate;
+  if (typeof timestamp == "number") {
+    const date = new Date(timestamp * 1000); // Multiply by 1000 to convert seconds to milliseconds
+    formattedDate = date.toLocaleString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+  } else {
+    const date = new Date("2024-12-18T18:45:03.150Z");
+    formattedDate = new Intl.DateTimeFormat("en-US", {
+      dateStyle: "medium",
+      timeStyle: "medium",
+    }).format(date);
+  }
+  return (
+    <p className={`${className} text-white font-Nunito text-base`}>
+      {formattedDate}
+    </p>
+  );
+};
+
+export default ConvertTimestamp;
