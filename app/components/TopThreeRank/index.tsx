286ebecf924d70cc016ae27a58814c9d53e7486a
MohammadMosavat <42.octmst@gmail.com>
2024-12-27 21:21:47 +0330

    Add TopThreeRank to show the top 3 of the meet

---
+++ app/components/TopThreeRank/index.tsx
@@ -0,0 +1,52 @@
+import ConvertTimestamp from "../ConvertTimesTamp";
+import { memo, useMemo } from "react";
+import GoldMedal from "/public/gold-medal.svg";
+import SilverMedal from "/public/silver-medal.svg";
+import BronzeMedal from "/public/bronze-medal.svg";
+import Image from "next/image";
+import NoiseEffect from "/public/noiseEffect2.svg?url";
+
+const TopThreeRank = ({
+  name,
+  joinedAt,
+  medal,
+  className,
+}: {
+  name: string;
+  joinedAt: string;
+  medal: "gold" | "silver" | "bronze";
+  className: string;
+}) => {
+  const validName = useMemo(() => {
+    return name.split("-")[0];
+  }, []);
+  const showMedal = useMemo(() => {
+    return medal == "gold" ? (
+      <GoldMedal className="absolute top-0 right-0 w-32 h-32" />
+    ) : medal == "silver" ? (
+      <SilverMedal className="absolute top-0 right-0 w-32 h-32" />
+    ) : (
+      <BronzeMedal className="absolute top-0 right-0 w-32 h-32" />
+    );
+  }, []);
+  return (
+    <div
+      className={`${className} [&>p]:max-md:text-sm bg-box-space relative flex z-0 flex-col gap-2 rounded-xl  p-4`}
+    >
+      <Image
+        className="absolute object-cover h-full w-full  z-10 bottom-0 top-0 left-0 "
+        width={5000}
+        height={5000}
+        src={NoiseEffect}
+        alt=""
+      />
+      {showMedal}
+      <h1 className="text-white font-SpaceGrotesk w-[63%] text-ellipsis overflow-hidden whitespace-nowrap text-xl">
+        {validName}
+      </h1>
+      <ConvertTimestamp time={joinedAt} />
+    </div>
+  );
+};
+
+export default memo(TopThreeRank);
