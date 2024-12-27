350e8c50d97f990507df321b63c1b8f9c0bf8f36
MohammadMosavat <42.octmst@gmail.com>
2024-12-27 21:17:48 +0330

    Add loading to space section

--- app/components/SpaceSection/index.tsx
+++ app/components/SpaceSection/index.tsx
@@ -1,45 +1,52 @@
-'use client'
-import useGetConfigData from '@/hooks/useGetConfig'
-import React from 'react'
-import SpaceCard from '../SpaceCard'
-import { dataType, SpaceSectionType } from '@/app/types'
+"use client";
+import useGetConfigData from "@/hooks/useGetConfig";
+import React, { useCallback } from "react";
+import SpaceCard from "../SpaceCard";
+import { dataType, SpaceSectionType } from "@/app/types";
+import Loading from "../Loading";
 
-function SpaceSection({search}: SpaceSectionType) {
-  const uiData = useGetConfigData(`/rooms/get-all-room-configs/sort?sort=all`)
-  console.log('This is search in space Section', search);
-
-  return (
-    <section className="grid mb-10 w-full col-span-full grid-cols-2 gap-4">
-      {
-        uiData?.map((data: dataType) => {
-          const regex = new RegExp(search, 'i'); 
+function SpaceSection({ search }: SpaceSectionType) {
+  const { configData, isLoading } = useGetConfigData(
+    `/rooms/get-all-room-configs/sort?sort=all`
+  );
+  
+  const showSpaces = useCallback(() => {
+    console.log("This is search in space Section", search, isLoading, configData);
+    return !isLoading && configData?.length > 0
+      ? configData.map((data: dataType) => {
+          const regex = new RegExp(search, "i");
           const matchesSearch = regex.test(data.slug);
-
-          return (
-            matchesSearch || search === ''  ? (
+  
+          if (matchesSearch || search === "") {
+            return (
               <SpaceCard
                 logo_URL={data?.config?.ui?.logo}
-                name_space={data?.slug || 'IM3'}
-                members={data?.config?.ui?.privateRoom ? data?.config?.whiteListParticipants.length : data?.config?.maxParticipants}
+                name_space={data?.slug || "IM3"}
+                members={
+                  data?.config?.ui?.privateRoom
+                    ? data?.config?.whiteListParticipants.length
+                    : data?.config?.maxParticipants
+                }
                 privateSpace={data?.config?.ui?.privateRoom}
                 className={"md:col-span-1 col-span-full"}
                 key={data.slug}
-                slug={data?.slug} roomUrl={data?.slug}
-                tags={data?.config?.ui?.tags} verified={data?.config?.verified}/>
-            ) : null
-          );
+                slug={data?.slug}
+                roomUrl={data?.slug}
+                tags={data?.config?.ui?.tags}
+                verified={data?.config?.verified}
+              />
+            );
+          }
+          return null;
         })
-      }
-      {/* <SpaceCard
-        logo_URL={'http://localhost:3000/api/v1/rooms/slugLogo/muon.svg'}
-        name_space={'PB'}
-        members={'20'}
-        privateSpace={true}
-        className={"md:col-span-1 col-span-full"}
-        slug={'pb'} roomUrl={'pb'}
-        tags={['web3']} verified={true}/> */}
+      : <Loading />;
+  }, [configData, isLoading, search]);
+  
+  return (
+    <section className="grid mb-10 w-full col-span-full grid-cols-2 gap-4">
+      {showSpaces()}
     </section>
-  )
+  );
 }
 
-export default SpaceSection
+export default SpaceSection;
