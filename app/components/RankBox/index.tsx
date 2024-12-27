1f96aa2bfde1b85130bb3c795911b4b2ca5e5115
MohammadMosavat <42.octmst@gmail.com>
2024-12-27 21:19:41 +0330

    Add rankBox component to show the participant of meet

---
+++ app/components/RankBox/index.tsx
@@ -0,0 +1,85 @@
+import { useMemo, useState } from "react";
+import ConvertTimestamp from "../ConvertTimesTamp";
+import useGetConfigData from "@/hooks/useGetConfig";
+
+interface RankBoxType {
+  user: {
+    rank: number;
+    name: string;
+    joinedAt: string | number;
+  };
+  permission: {
+    canSubscribe: boolean;
+    canPublish: boolean;
+    canPublishData: boolean;
+    recorder: boolean;
+  };
+}
+const RankBox = ({ user, permission }: RankBoxType) => {
+  
+  const [activeSlug, setActiveSlug] = useState<boolean>(false);
+  const toggleInfoBox = () => {
+    setActiveSlug(!activeSlug);
+  };
+
+  const showInfo = useMemo(() => {
+    return (
+      activeSlug && (
+        <section className="flex bg-box-space p-2 rounded-lg max-md:flex-col md:items-center gap-2">
+          {permission.canPublish == true && (
+            <p className="text-white font-SpaceGrotesk select-none capitalize">
+              Can subscribe
+            </p>
+          )}
+          {permission.canPublish == true && (
+            <p className="text-white font-SpaceGrotesk select-none capitalize">
+              Can Publish
+            </p>
+          )}
+          {permission.canPublishData == true && (
+            <p className="text-white font-SpaceGrotesk select-none capitalize">
+              Can Publish Data
+            </p>
+          )}
+          {permission.recorder == true && (
+            <p className="text-white font-SpaceGrotesk select-none capitalize">
+              recorder
+            </p>
+          )}
+          <ConvertTimestamp time={user.joinedAt} />
+        </section>
+      )
+    );
+  }, [activeSlug]);
+
+  const valueButton = useMemo(() => {
+    return activeSlug ? "Less" : "More";
+  }, [activeSlug]);
+  return (
+    <li className="flex flex-col justify-center gap-4 ">
+      <div className="flex items-center justify-between w-full">
+        <div className="flex gap-2 items-center">
+          <section className="flex items-center gap-2">
+            <p className="font-SpaceGrotesk text-base text-white bg-box-space px-2 rounded-full">
+              {user.rank + 1}
+            </p>
+            <h1 className="text-white max-md:w-[170px] overflow-hidden whitespace-nowrap text-ellipsis text-base font-SpaceGrotesk">
+              {user.name}
+            </h1>
+          </section>
+        </div>
+        <section className="flex items-center justify-between gap-2 [&>p]:text-base">
+          <button
+            className="text-white text-sm font-SpaceGrotesk bg-box-space px-2 rounded-md"
+            onClick={toggleInfoBox}
+          >
+            {valueButton}
+          </button>
+        </section>
+      </div>
+      {showInfo}
+    </li>
+  );
+};
+
+export default RankBox;
