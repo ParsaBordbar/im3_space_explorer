5b73c633a9e53dfc0a84a9489bf63729c35d2bb4
MohammadMosavat <42.octmst@gmail.com>
2024-12-27 21:16:56 +0330

    Update the useGetConfig

--- hooks/useGetConfig.ts
+++ hooks/useGetConfig.ts
@@ -1,24 +1,36 @@
 import { useEffect, useState } from "react";
 import { spaceApi } from "../api/base";
 
-
 const useGetConfigData = (slug: string) => {
-    const [ configData, setConfigData ] = useState<any>();
-    
-    useEffect(() => {
-        fetchConfigData(slug);
-    }, [])
-    
-    const fetchConfigData = async (slug: string) => {
-        try {
-            const response = await spaceApi.get(slug);
-            setConfigData(response.data);
-            console.log("This is Response from Hook, ", response);
-         } catch (error) {
-             console.log(error);
-         }
+  const [configData, setConfigData] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const fetchConfigData = async (slug: string) => {
+    setIsLoading(true);
+    try {
+      const response = await spaceApi.get(slug);
+      Array.isArray(response.data) && response.data.length === 0
+        ? setConfigData(0)
+        : setConfigData(response.data);
+      console.log("Response from Hook:", response, slug);
+    } catch (err) {
+      console.error("Error fetching config data:", err);
+      setError(
+        err instanceof Error ? err.message : "An unknown error occurred"
+      );
+    } finally {
+      setIsLoading(false);
     }
-    return configData;
-}
+  };
+
+  useEffect(() => {
+    if (slug) {
+      fetchConfigData(slug);
+    }
+  }, [slug]);
+
+  return { configData, error, isLoading };
+};
 
 export default useGetConfigData;
