8ef412bcd3324b356176bf8bbca1f12c556087d5
MohammadMosavat <42.octmst@gmail.com>
2025-01-11 19:46:00 +0330

    update the tab component

--- app/components/Tab/index.tsx
+++ app/components/Tab/index.tsx
@@ -1,13 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 interface TabProps {
-  title: string;
+  tabName: string;
   className?: string;
-  children: React.ReactNode;
+  link: string;
 }
 
-const Tab = ({ title, children, className }: TabProps) => {
-  return <div className={`${className}`}>{children}</div>;
+const Tab = ({ tabName, className, link }: TabProps) => {
+  return (
+    <Link
+      href={`${link}`}
+      className={`${className} text-white cursor-pointer max-sm:text-base !rounded-[11px] bg-[#5b5b5d3e] transition-all ease-in-out duration-200 font-Nunito text-lg font-bold py-2 px-4 `}
+    >
+      {tabName}
+    </Link>
+  );
 };
 
 export default Tab;
